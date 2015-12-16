dd15caf2b2f8e1e1bb35f9b2739f807d846a442a
Alberto Ferreira De Souza <alberto@lcad.inf.ufes.br>
2015-12-16 10:14:12 -0200

    improvements in cyton_alpha for real robot operation

--- cyton_alpha_driver/scripts/cyton_driver.py
+++ cyton_alpha_driver/scripts/cyton_driver.py
@@ -28,10 +28,11 @@ class CytonAction(object):
 
   def __init__(self, name):
     self._action_name = name
-    self._names = ['claw_left', 'claw_right', 'elbow_pitch', 'shoulder_base', 'shoulder_pitch', 'shoulder_yaw', 'wrist_pitch', 'wrist_roll', 'wrist_yaw']
+    print name
+    self._names = ['elbow_pitch', 'shoulder_base', 'shoulder_pitch', 'shoulder_yaw', 'wrist_pitch', 'wrist_roll', 'wrist_yaw']
     self._state = JointState(name = self._names, position = [0]*len(self._names))
     # print self._state.position
-    self._joint_limits = {'shoulder_base':{'min':0,'max':1.57,'vel':.4}}
+    # self._joint_limits = {'shoulder_base':{'min':0,'max':1.57,'vel':.4}}
     # print self._joint_limits
     self._as = actionlib.SimpleActionServer(self._action_name, FollowJointTrajectoryAction, execute_cb=self.execute_cb, auto_start = False)
     self._as.start()
@@ -52,17 +53,6 @@ class CytonAction(object):
     #self.servo.setTarget(1, servo_value)
 
 
-  def handle_unigripper_vacuum(self, req):
-    if req.TurnVacuumOn == True:
-      #self.servo.setTarget(0, 7600)
-      print "Turned vacuum on"
-      return UnigripperVacuumOnResponse(True)
-    else:
-      #self.servo.setTarget(0, 4000)
-      print "Turned vacuum off"
-      return UnigripperVacuumOnResponse(False)
-    
-
   def execute_cb(self, goal):
     rospy.loginfo('Received Trajectory')
     ## helper variables
@@ -82,29 +72,29 @@ class CytonAction(object):
         success = False
         break
 
-    for outer_loop in xrange(1,len(trajectory.points)):
-      target_point = trajectory.points[outer_loop]
-      for x in xrange(len(trajectory.joint_names)):
-        minimum = self._joint_limits[trajectory.joint_names[x]]['min']
-        maximum = self._joint_limits[trajectory.joint_names[x]]['max']
-        if target_point.positions[x] < minimum or target_point.positions[x] > maximum:
-          rospy.logerr('%s joint position is outside limits. Aborting...',trajectory.joint_names[x])
-          success = False
-          break
+#    for outer_loop in xrange(1,len(trajectory.points)):
+#      target_point = trajectory.points[outer_loop]
+#      for x in xrange(len(trajectory.joint_names)):
+#        minimum = self._joint_limits[trajectory.joint_names[x]]['min']
+#        maximum = self._joint_limits[trajectory.joint_names[x]]['max']
+#        if target_point.positions[x] < minimum or target_point.positions[x] > maximum:
+#          rospy.logerr('%s joint position is outside limits. Aborting...',trajectory.joint_names[x])
+#          success = False
+#          break
       
       diff = (target_point.time_from_start - current_point.time_from_start)
-      for x in xrange(len(trajectory.joint_names)):
-        max_vel = self._joint_limits[trajectory.joint_names[x]]['vel']
-        calculated_vel = (target_point.positions[x] - current_point.positions[x]) / (diff.secs + diff.nsecs / 1000000000.0)
-        if math.fabs(calculated_vel) > max_vel:            
-          print (diff.secs + diff.nsecs / 1000000000.0)
-          print (target_point.positions[x] - current_point.positions[x])
-          print target_point.positions[x]
-          print current_point.positions[x]
-          rospy.logerr('Unigripper driver:  %s requested velocity %f (given by the time_from_start value) is too large (max %f). Aborting...', trajectory.joint_names[x], calculated_vel, max_vel)
-          success = False
-          break
-
+#      for x in xrange(len(trajectory.joint_names)):
+#        max_vel = self._joint_limits[trajectory.joint_names[x]]['vel']
+#        calculated_vel = (target_point.positions[x] - current_point.positions[x]) / (diff.secs + diff.nsecs / 1000000000.0)
+#        if math.fabs(calculated_vel) > max_vel:            
+#          print (diff.secs + diff.nsecs / 1000000000.0)
+#          print (target_point.positions[x] - current_point.positions[x])
+#          print target_point.positions[x]
+#          print current_point.positions[x]
+#          rospy.logerr('Unigripper driver:  %s requested velocity %f (given by the time_from_start value) is too large (max %f). Aborting...', trajectory.joint_names[x], calculated_vel, max_vel)
+#          success = False
+#          break
+#
       if not success:
         break
     
@@ -133,19 +123,15 @@ class CytonAction(object):
       
             
 if __name__ == '__main__':
-  rospy.init_node('cyton_joint_path_command')
+  rospy.init_node('arm_controller')
 
-  #action = CytonAction("cyton" + rospy.get_name())
-  action = CytonAction("cyton" + rospy.get_name())
+  action = CytonAction(rospy.get_name())
 
   pub = rospy.Publisher('/joint_states', JointState, queue_size=2)
-  #s = rospy.Service('unigripper_vacuum', UnigripperVacuumOn, action.handle_unigripper_vacuum)
   rate = rospy.Rate(10) # 10hz
-  # robotiq_state = JointState(name=['finger_1_joint_1', 'finger_1_joint_2', 'finger_1_joint_3', 'finger_2_joint_1', 'finger_2_joint_2', 'finger_2_joint_3', 'finger_middle_joint_1', 'finger_middle_joint_2', 'finger_middle_joint_3', 'palm_finger_1_joint', 'palm_finger_2_joint'], position=[0]*11)
-  ##publish the state from the MotomanAction 
+
   while not rospy.is_shutdown():
     action._state.header.stamp = rospy.Time.now()
     pub.publish(action._state)
-    # robotiq_state.header.stamp = rospy.Time.now()
-    # pub.publish(robotiq_state)
     rate.sleep()
+
