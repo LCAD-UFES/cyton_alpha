b0c3dd45fc63bfee5606be4d8c49874d1e048288
vinibc <viniciusbc1@gmail.com>
2016-02-08 20:00:25 -0200

    New gripper_driver and SRDF changes to enable control joints

--- cyton_alpha_driver/scripts/cyton_gripper_follow.py
+++ cyton_alpha_driver/scripts/cyton_gripper_follow.py
@@ -4,6 +4,8 @@ import roslib
 import rospy
 import math
 
+from cyton.msg import cytonAction, cytonActionGoal, cytonGoal
+
 import actionlib
 
 # import actionlib_tutorials.msg
@@ -20,39 +22,42 @@ from control_msgs.msg import (
 #from servo_device import Controller
 #from unigripper_msgs.srv import *
 
-
-class CytonAction(object):
+#
+#Action server to receive MoveIt goals and 
+#Action client to send to cyton hardware_node
+#
+class CytonActionServer(object):
   # create messages that are used to publish feedback/result
   _feedback = FollowJointTrajectoryFeedback()
   _result   = FollowJointTrajectoryResult()
+  _cyton_goal = cytonGoal()
 
   def __init__(self, name):
     self._action_name = name
     print name
-    self._names = ['elbow_pitch', 'shoulder_base', 'shoulder_pitch', 'shoulder_yaw', 'wrist_pitch', 'wrist_roll', 'wrist_yaw']
+    self._names = ['claw_left', 'claw_right']
     self._state = JointState(name = self._names, position = [0]*len(self._names))
     print self._state.position
     # self._joint_limits = {'shoulder_base':{'min':0,'max':1.57,'vel':.4}}
     # print self._joint_limits
     self._as = actionlib.SimpleActionServer(self._action_name, FollowJointTrajectoryAction, execute_cb=self.execute_cb, auto_start = False)
     self._as.start()
-
-    #self.servo = Controller()
-    #self.servo.setAccel(1, 0) # hinge servo
-    #self.servo.setSpeed(1, 0) # hinge servo
-    #self.set_servo_state(self._state.position[0]) # head_hinge
+    
+    #cyton action client
+    #self.client = actionlib.SimpleActionClient('cyton', cytonAction)
+    #rospy.loginfo('Cyton Action Client start.')    
+ 
     print "@@@@@@@@@@@@@ Initial head_hinge position = ", self._state.position[0]
 
   def execute_cb(self, goal):
     rospy.loginfo('Received Trajectory')
 #     ## helper variables
-    r = rospy.Rate(10)
+    r = rospy.Rate(20)
     success = True
 #    print "I'm executing and I got the goal", goal
         
     trajectory = goal.trajectory
     current_point = trajectory.points[0]
-    self._state.position = trajectory.points
      
      # start executing the action
     for i in xrange(0, len(trajectory.points)):
@@ -62,7 +67,16 @@ class CytonAction(object):
         self._as.set_preempted()
         success = False
         break
-      self._state.position = trajectory.points[i].positions
+      
+      print "Gripper Controller: Executando:[%f,%f]" % (trajectory.points[i].positions[0],trajectory.points[i].positions[1])  
+      self._state.position[0] = trajectory.points[i].positions[0]
+      self._state.position[1] = trajectory.points[i].positions[1]
+      
+      #rospy.loginfo('wait Hardware node start...')
+      #self.client.wait_for_server()
+      #rospy.loginfo('Hardware node started.')
+      #self.client.send_goal(self._cyton_goal)
+      #finished_before_timeout = self.client.wait_for_result(rospy.Duration(1))
       # publish the feedback
       # this step is not necessary, the sequence is computed at 1 Hz for demonstration purposes
       r.sleep()
@@ -75,9 +89,9 @@ class CytonAction(object):
       
 #end execute_cb
 if __name__ == '__main__':
-  rospy.init_node('arm_controller')
+  rospy.init_node('cyton_gripper_driver')
 
-  action = CytonAction('/arm_controller/follow_joint_trajectory')
+  action = CytonActionServer('/claw_controller/gripper_action')
 
   pub = rospy.Publisher('/joint_states', JointState, queue_size=2)
   rate = rospy.Rate(10) # 10hz
